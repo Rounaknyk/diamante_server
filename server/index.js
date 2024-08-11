a72b92f8902205ceefee7b4dc03e33384b3971de
Rounak Naik <rounakpro18@gmail.com>
2024-08-11 13:22:13 +0530

    latest one

--- server/index.js
+++ server/index.js
@@ -41,7 +41,7 @@ app.use(testRouter);
 // console.log(secret);
 
 const port = 3000;
-const serverIp = '192.0.0.2';
+//const serverIp = '192.0.0.2';
 // const serverIp = '192.168.102.75';
 // const serverIp = '64.71.146.212';
 
