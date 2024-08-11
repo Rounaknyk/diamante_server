44cbf423844ec0748d7d6fa250c47df6530fb2fa
Rounak Naik <rounakpro18@gmail.com>
2024-08-11 15:40:44 +0530

    latest one

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "ce62e490f88add07e41dce0d5dfd745e",
+const RESOURCES = {"flutter_bootstrap.js": "920780ef3691fc09e391ca7098d93bd8",
 "version.json": "b24e5109a0c99f602bcdf5a31b03ecae",
 "index.html": "986bcfa37abe9df9748915001fdfc38f",
 "/": "986bcfa37abe9df9748915001fdfc38f",
